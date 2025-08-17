181f4afed95d303c509e92f468ccf07765883187
xac <jiangfengxi.c@gmail.com>
2025-08-18 00:00:27 +0800

    [refactor] Add utility functions for FormBase to manage components

--- UI/FormBase.cs
+++ UI/FormBase.cs
@@ -25,7 +25,51 @@ namespace Xuf.UI
         }
     }
 
-    public abstract class FormBase<T> : UIBehaviour
+    public abstract class FormBase : UIBehaviour
+    {
+        //SECTION: Utility Functions
+        protected static TComp GetOrAddComponent<TComp>(GameObject gameObject)
+            where TComp : Component
+        {
+            var component = gameObject.GetComponent<TComp>();
+            if (component == null)
+            {
+                component = gameObject.AddComponent<TComp>();
+            }
+            return component;
+        }
+
+        protected static void ClearTemplateChildren(Transform parent, GameObject template)
+        {
+            template.SetActive(false);
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                if (child.gameObject != template)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
+            }
+        }
+
+        protected static void SetActiveAllChildren(Transform parent, bool active, bool recursive = false)
+        {
+            if (parent == null) return;
+
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var child = parent.GetChild(i);
+                child.gameObject.SetActive(active);
+
+                if (recursive)
+                {
+                    SetActiveAllChildren(child, active, true);
+                }
+            }
+        }
+    }
+
+    public abstract class FormBase<T> : FormBase
     {
         public T m_data;
 
